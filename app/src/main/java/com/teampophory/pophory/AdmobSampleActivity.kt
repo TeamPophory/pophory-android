c27f84b8f6c7b49e072bb4d881091f3dcc2c4faf
곽의진 <yuijin0909@gmail.com>
2023-06-28 01:12:19 +0900

    [feature/admob]: Sample Activity 추가 로직 원복

--- app/src/main/java/com/teampophory/pophory/AdmobSampleActivity.kt
+++ app/src/main/java/com/teampophory/pophory/AdmobSampleActivity.kt
@@ -3,6 +3,7 @@ package com.teampophory.pophory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.teampophory.pophory.databinding.ActivityAdmobSampleBinding
+import com.teampophory.pophory.util.ads.AdmobService
 
 class AdmobSampleActivity : AppCompatActivity() {
         private val binding by lazy {
@@ -10,7 +11,10 @@ class AdmobSampleActivity : AppCompatActivity() {
         }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admob_sample)
+        setContentView(binding.root)
 
+        binding.root.setOnClickListener {
+            AdmobService().getNativeAd(this, binding.adContainer)
+        }
     }
 }
