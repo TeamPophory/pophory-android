62ebe6368a65b63312f9ba0f57b030358efea6f1
Hyun-Woo Lee <54518925+l2hyunwoo@users.noreply.github.com>
2023-06-27 22:29:43 +0900

    [feature/github-configuration] Github 환경 설정 (#6)
    * :construction_worker: Add build health check system and add CODEOWNERS, auto-assign
    
    * :heavy_plus_sign: Add kotlin dependencies, shrink app gradle scripts
    
    * :bug: remove local properties load
    
    * :green_heart: android ci config fix
    
    * [docs] : PR template create
    
    ---------
    
    Co-authored-by: librarywon <librarywon@naver.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,45 +1,25 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.teampophory.pophory.application")
 }
 
 android {
     namespace = "com.teampophory.pophory"
-    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.teampophory.pophory"
-        minSdk = 28
-        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-
-    implementation(libs.core.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
 }
--- build-logic/convention/src/main/kotlin/com/teampophory/pophory/plugin/CommonConfigs.kt
+++ build-logic/convention/src/main/kotlin/com/teampophory/pophory/plugin/CommonConfigs.kt
@@ -9,10 +9,6 @@ import org.gradle.kotlin.dsl.getByType
 import java.util.Properties
 
 internal fun Project.configureAndroidCommonPlugin() {
-    val properties = Properties().apply {
-        load(rootProject.file("local.properties").inputStream())
-    }
-
     apply<AndroidKotlinPlugin>()
     apply<KotlinSerializationPlugin>()
     with(plugins) {
