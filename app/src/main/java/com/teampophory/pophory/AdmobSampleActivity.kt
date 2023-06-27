7bf21ea255b25dcf749202875f42316db48db51a
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-06-28 01:45:11 +0900

    :recycle: Set adUnitId, adContainer to props for dependency injection

--- app/src/main/java/com/teampophory/pophory/AdmobSampleActivity.kt
+++ app/src/main/java/com/teampophory/pophory/AdmobSampleActivity.kt
@@ -1,19 +1,10 @@
 package com.teampophory.pophory
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import coil.load
-import com.google.android.gms.ads.nativead.NativeAd
-import com.google.android.gms.ads.nativead.NativeAdView
 import com.teampophory.pophory.databinding.ActivityAdmobSampleBinding
-import com.teampophory.pophory.util.ads.AdmobService
+import com.teampophory.pophory.util.ads.AdmobServiceFactory
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -23,49 +14,19 @@ class AdmobSampleActivity : AppCompatActivity() {
     }
 
     @Inject
-    lateinit var admobService: AdmobService
+    lateinit var admobServiceFactory: AdmobServiceFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val service = admobServiceFactory.create(
+            adUnitId = "ca-app-pub-3940256099942544/2247696110",
+            adContainer = binding.adContainer
+        )
+
         binding.root.setOnClickListener {
-            lifecycleScope.launch {
-                admobService.getNativeAd()?.let { nativeAd ->
-                    binding.adContainer.apply {
-                        removeAllViews()
-                        addView(nativeAd.toNativeAdView())
-                    }
-                }
-            }
-        }
-    }
-
-    private fun NativeAd.toNativeAdView(): NativeAdView? {
-        return try {
-            layoutInflater.inflate(R.layout.admob_native_default, null)
-                .let { nativeAdView ->
-                    nativeAdView as? NativeAdView
-                }?.apply {
-                    setViewContent(this, this@toNativeAdView)
-                    setNativeAd(this@toNativeAdView)
-                }
-        } catch (e: Exception) {
-            Log.e("ADS_ERROR", e.toString())
-            null
-        }
-    }
-
-    private fun setViewContent(nativeAdView: NativeAdView, nativeAd: NativeAd) {
-        nativeAdView.apply {
-            iconView = findViewById(R.id.ad_app_icon)
-            headlineView = findViewById(R.id.ad_headline)
-            bodyView = findViewById(R.id.ad_body)
-            callToActionView = findViewById(R.id.ad_call_to_action)
-            (headlineView as? TextView)?.text = nativeAd.headline
-            (bodyView as? TextView)?.text = nativeAd.body
-            (callToActionView as? Button)?.text = nativeAd.callToAction
-            (iconView as? ImageView)?.load(nativeAd.icon?.uri)
+            service.getNativeAd()
         }
     }
 }
--- app/src/main/java/com/teampophory/pophory/util/ads/AdmobService.kt
+++ app/src/main/java/com/teampophory/pophory/util/ads/AdmobService.kt
@@ -1,29 +1,79 @@
 package com.teampophory.pophory.util.ads
 
 import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
+import coil.load
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdLoader
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.nativead.NativeAd
+import com.google.android.gms.ads.nativead.NativeAdView
+import com.teampophory.pophory.R
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import dagger.hilt.android.qualifiers.ActivityContext
-import kotlinx.coroutines.suspendCancellableCoroutine
-import javax.inject.Inject
-import kotlin.coroutines.resumeWithException
 
-class AdmobService @Inject constructor(
-    @ActivityContext private val context: Context
+class AdmobService @AssistedInject constructor(
+    @ActivityContext private val context: Context,
+    @Assisted private val adUnitId: String,
+    @Assisted private val adContainer: FrameLayout
 ) {
-    suspend fun getNativeAd() = suspendCancellableCoroutine {
-        val adLoader = AdLoader.Builder(context, "ca-app-pub-3940256099942544/2247696110")
+    fun getNativeAd() {
+        val adLoader = AdLoader.Builder(context, adUnitId)
             .forNativeAd { ad: NativeAd? ->
-                it.resumeWith(Result.success(ad))
+                ad?.let { nativeAd ->
+                    adContainer.apply {
+                        removeAllViews()
+                        addView(nativeAd.toNativeAdView(context))
+                    }
+                }
             }.withAdListener(object : AdListener() {
                 override fun onAdFailedToLoad(adError: LoadAdError) {
-                    it.resumeWithException(Throwable(adError.toString()))
+                    Log.e("ADS_ERROR", adError.toString())
                 }
             }).build()
 
         adLoader.loadAd(AdRequest.Builder().build())
     }
+
+    private fun NativeAd.toNativeAdView(context: Context): NativeAdView? {
+        return try {
+            LayoutInflater.from(context)
+                .inflate(R.layout.admob_native_default, null)
+                .let { nativeAdView ->
+                    nativeAdView as? NativeAdView
+                }?.apply {
+                    setViewContent(this, this@toNativeAdView)
+                    setNativeAd(this@toNativeAdView)
+                }
+        } catch (e: Exception) {
+            Log.e("ADS_ERROR", e.toString())
+            null
+        }
+    }
+
+    private fun setViewContent(nativeAdView: NativeAdView, nativeAd: NativeAd) {
+        nativeAdView.apply {
+            iconView = findViewById(R.id.ad_app_icon)
+            headlineView = findViewById(R.id.ad_headline)
+            bodyView = findViewById(R.id.ad_body)
+            callToActionView = findViewById(R.id.ad_call_to_action)
+            (headlineView as? TextView)?.text = nativeAd.headline
+            (bodyView as? TextView)?.text = nativeAd.body
+            (callToActionView as? Button)?.text = nativeAd.callToAction
+            (iconView as? ImageView)?.load(nativeAd.icon?.uri)
+        }
+    }
+}
+
+@AssistedFactory
+interface AdmobServiceFactory {
+    fun create(adUnitId: String, adContainer: FrameLayout): AdmobService
 }
