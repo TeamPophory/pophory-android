64af34472d08355206945821a874cce7024b3b47
곽의진 <yuijin0909@gmail.com>
2023-06-28 01:05:39 +0900

    [feature/admob]: admob SDK 탑재 및 native 추가

--- app/src/main/java/com/teampophory/pophory/App.kt
+++ app/src/main/java/com/teampophory/pophory/App.kt
@@ -1,7 +1,13 @@
 package com.teampophory.pophory
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        MobileAds.initialize(this) {}
+    }
+}
---
+++ app/src/main/java/com/teampophory/pophory/util/ads/AdmobService.kt
@@ -0,0 +1,69 @@
+package com.teampophory.pophory.util.ads
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
+import coil.load
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdLoader
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.nativead.NativeAd
+import com.google.android.gms.ads.nativead.NativeAdView
+import com.teampophory.pophory.R
+
+
+class AdmobService {
+    fun getNativeAd(context: Context, frameLayout: FrameLayout) {
+        val adLoader = AdLoader.Builder(context, "ca-app-pub-3940256099942544/2247696110")
+            .forNativeAd { ad: NativeAd? ->
+                ad?.let { nativeAd ->
+                    frameLayout.apply {
+                        removeAllViews()
+                        addView(nativeAd.toNativeAdView(context))
+                    }
+                }
+            }.withAdListener(object : AdListener() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    Log.e("ADS_ERROR", adError.toString())
+                }
+            }).build()
+
+        adLoader.loadAd(AdRequest.Builder().build())
+    }
+
+    private fun NativeAd.toNativeAdView(context: Context): NativeAdView? {
+        return try {
+            LayoutInflater.from(context)
+                .inflate(R.layout.admob_native_default, null)
+                .let { nativeAdView ->
+                    nativeAdView as? NativeAdView
+                }?.apply {
+                    setViewContent(this, this@toNativeAdView)
+                    setNativeAd(this@toNativeAdView)
+                }
+        } catch (e: Exception) {
+            Log.e("ADS_ERROR", e.toString())
+            null
+        }
+    }
+
+    private fun setViewContent(nativeAdView: NativeAdView, nativeAd: NativeAd) {
+        nativeAdView.apply {
+            iconView = findViewById(R.id.ad_app_icon)
+            headlineView = findViewById(R.id.ad_headline)
+            bodyView = findViewById(R.id.ad_body)
+            callToActionView = findViewById(R.id.ad_call_to_action)
+            (headlineView as? TextView)?.text = nativeAd.headline
+            (bodyView as? TextView)?.text = nativeAd.body
+            (callToActionView as? Button)?.text = nativeAd.callToAction
+            (iconView as? ImageView)?.load(nativeAd.icon?.uri)
+        }
+    }
+}
