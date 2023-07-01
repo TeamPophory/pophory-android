d6e3f904723df1f4340d127e3b6d8db914d41674
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-02 01:35:35 +0900

    Fragment ViewBinding 적용

--- app/src/main/java/com/teampophory/pophory/feature/store/StoreFragment.kt
+++ app/src/main/java/com/teampophory/pophory/feature/store/StoreFragment.kt
@@ -5,28 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.teampophory.pophory.common.view.viewBinding
 import com.teampophory.pophory.databinding.FragmentStoreBinding
 
 class StoreFragment : Fragment() {
-    private var _binding: FragmentStoreBinding? = null
-    private val binding: FragmentStoreBinding
-        get() = requireNotNull(_binding) { "binding is null" }
+    private val binding by viewBinding(FragmentStoreBinding::bind)
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentStoreBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
