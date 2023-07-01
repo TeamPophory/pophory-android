ca6b0f9d89e444dfea765ba483729ab8c43bf98b
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-01 23:54:35 +0900

    Timber Initialization

---
+++ app/src/main/java/com/teampophory/pophory/config/initializer/CrashlyticsInitializer.kt
@@ -0,0 +1,15 @@
+package com.teampophory.pophory.config.initializer
+
+import android.content.Context
+import androidx.startup.Initializer
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+
+class CrashlyticsInitializer: Initializer<FirebaseCrashlytics> {
+    override fun create(context: Context): FirebaseCrashlytics {
+        return FirebaseCrashlytics.getInstance()
+    }
+
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> {
+        return mutableListOf()
+    }
+}
