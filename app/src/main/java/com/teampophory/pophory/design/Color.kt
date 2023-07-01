f29dd17be60fe454c876a48b60bab2e8ad730775
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-02 03:42:21 +0900

    Compose PophoryTheme 설정

---
+++ app/src/main/java/com/teampophory/pophory/design/Color.kt
@@ -0,0 +1,16 @@
+package com.teampophory.pophory.design
+
+import androidx.compose.ui.graphics.Color
+
+// Purple Scale
+val Purple = Color(0xFF725BFF)
+
+// Red Scale
+val Red = Color(0xFFFC4646)
+
+// Gray Scale
+val White = Color(0xFFFFFFFF)
+val Gray400 = Color(0xFF212121)
+val Gray300 = Color(0xFF5F6168)
+val Gray200 = Color(0xFFB8B9BE)
+val Gray100 = Color(0xFFE0E1E5)
