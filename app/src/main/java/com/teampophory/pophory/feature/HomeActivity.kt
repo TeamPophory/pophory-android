79537c5372f3fc0914f3d3b30edf6efc298c5ef8
librarywon <librarywon@naver.com>
2023-07-01 17:32:31 +0900

    [feat/bottomappbar]: add bottom nav with fav

---
+++ app/src/main/java/com/teampophory/pophory/feature/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.teampophory.pophory.feature
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.teampophory.pophory.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+}
