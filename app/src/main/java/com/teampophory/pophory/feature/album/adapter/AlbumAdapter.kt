02bb23b7b740bea3350a80cc848276c63547c618
곽의진 <yuijin0909@gmail.com>
2023-07-02 01:54:25 +0900

    [fix/flexbox]: Change DiffUtil

--- app/src/main/java/com/teampophory/pophory/feature/album/adapter/AlbumAdapter.kt
+++ app/src/main/java/com/teampophory/pophory/feature/album/adapter/AlbumAdapter.kt
@@ -3,12 +3,15 @@ package com.teampophory.pophory.feature.album.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import com.teampophory.pophory.common.view.ItemDiffCallback
 import com.teampophory.pophory.databinding.ItemFlexboxLayoutBinding
 import com.teampophory.pophory.feature.album.viewholder.FlexBoxLayoutViewHolder
-import com.teampophory.pophory.util.DiffCallback
 
 class AlbumAdapter : ListAdapter<Int, FlexBoxLayoutViewHolder>(
-    DiffCallback<Int>(id = { old, new -> old == new })
+    ItemDiffCallback<Int>(
+        onItemsTheSame = { old, new -> old == new },
+        onContentsTheSame = { old, new -> old == new }
+    )
 ) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlexBoxLayoutViewHolder {
