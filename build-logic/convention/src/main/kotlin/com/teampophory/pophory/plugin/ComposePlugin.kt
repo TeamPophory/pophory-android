a08b9f8ffe5dfa8759e70f97e3b6f09f44e40aaf
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-02 03:16:28 +0900

    Compose Plugin 설정

---
+++ build-logic/convention/src/main/kotlin/com/teampophory/pophory/plugin/ComposePlugin.kt
@@ -0,0 +1,27 @@
+package com.teampophory.pophory.plugin
+
+import com.android.build.gradle.BaseExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+class ComposePlugin : Plugin<Project> {
+    override fun apply(target: Project) = with(target) {
+        val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+        extensions.getByType<BaseExtension>().apply {
+            buildFeatures.apply {
+                compose = true
+            }
+            composeOptions {
+                kotlinCompilerExtensionVersion = "1.4.8"
+            }
+        }
+
+        dependencies {
+            "implementation"(platform(libs.findLibrary("compose.bom").get()))
+            "implementation"(libs.findBundle("compose").get())
+        }
+    }
+}
