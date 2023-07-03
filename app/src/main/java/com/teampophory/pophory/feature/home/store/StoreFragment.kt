ba34282987b06fcd421cb091acfac80185cb348d
librarywon <librarywon@naver.com>
2023-07-03 12:49:04 +0900

    [feat/store]: add SpannableString

--- app/src/main/java/com/teampophory/pophory/feature/home/store/StoreFragment.kt
+++ app/src/main/java/com/teampophory/pophory/feature/home/store/StoreFragment.kt
@@ -2,9 +2,13 @@ package com.teampophory.pophory.feature.home.store
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -32,6 +36,7 @@ class StoreFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setupViewPager()
         observeAlbumList()
+        setSpannableString()
     }
 
     private fun setupViewPager() {
@@ -55,5 +60,25 @@ class StoreFragment : Fragment() {
         viewModel.albumList.observe(viewLifecycleOwner, Observer { list ->
             adapter?.submitList(list)
         })
+    }
+
+    private fun setSpannableString() {
+        val fullText = getString(R.string.store_welcome)
+        val coloredText = "포포링만의 추억을" // 색상을 변경하려는 특정 단어
+
+        val spannableStringBuilder = SpannableStringBuilder(fullText)
+        val start = fullText.indexOf(coloredText)
+        val end = start + coloredText.length
+
+        if (start != -1) {
+            spannableStringBuilder.setSpan(
+                ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.pophory_purple)),
+                start,
+                end,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+
+        binding.tvStoreWelcome.text = spannableStringBuilder
     }
 }
