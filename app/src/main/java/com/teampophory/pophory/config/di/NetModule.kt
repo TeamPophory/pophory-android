46658c9c94534f7debb929f375f500ec4ab5e296
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-04 18:47:04 +0900

    Interceptor 정의하고 NetModule 구축

---
+++ app/src/main/java/com/teampophory/pophory/config/di/NetModule.kt
@@ -0,0 +1,62 @@
+package com.teampophory.pophory.config.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.teampophory.pophory.config.di.qualifier.Auth
+import com.teampophory.pophory.config.di.qualifier.Log
+import com.teampophory.pophory.data.network.AuthInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetModule {
+    private const val KAKAO_API_BASE_URL = "https://kapi.kakao.com/"
+    @Singleton
+    @Provides
+    fun provideJsonConverterFactory(): Converter.Factory {
+        return Json.asConverterFactory("application/json".toMediaType())
+    }
+
+    @Singleton
+    @Provides
+    @Log
+    fun provideLoggingInterceptor(): Interceptor =
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Singleton
+    @Provides
+    @Auth
+    fun provideAuthInterceptor(interceptor: AuthInterceptor): Interceptor = interceptor
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        @Log logInterceptor: Interceptor,
+        @Auth authInterceptor: Interceptor
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(logInterceptor)
+        .addInterceptor(authInterceptor)
+        .build()
+
+    // TODO by Nunu BaseUrl 변경
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        client: OkHttpClient,
+        converterFactory: Converter.Factory
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(KAKAO_API_BASE_URL)
+        .client(client)
+        .addConverterFactory(converterFactory)
+        .build()
+}
