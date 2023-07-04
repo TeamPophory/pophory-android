adfd16b9fd31c265f4f543ca11e9ec018ad642b5
librarywon <librarywon@naver.com>
2023-07-05 01:50:52 +0900

    [feat/mypage]: fix responsenamig

--- app/src/main/java/com/teampophory/pophory/data/repository/fake/FakeMyPageInfoRepository.kt
+++ app/src/main/java/com/teampophory/pophory/data/repository/fake/FakeMyPageInfoRepository.kt
@@ -1,106 +1,23 @@
 package com.teampophory.pophory.data.repository.fake
 
 import com.teampophory.pophory.data.repository.MyPageInfoRepository
-import com.teampophory.pophory.network.model.MyPageInfoResponse
+import com.teampophory.pophory.network.model.MyPageResponse
 import kotlinx.coroutines.delay
 
 class FakeMyPageInfoRepository : MyPageInfoRepository {
     private val fakeImageUrl =
         "https://images.unsplash.com/photo-1687023956422-117d5c47029d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3432&q=80"
 
-    override suspend fun getMyPageInfo(): Result<MyPageInfoResponse> {
+    override suspend fun getMyPageInfo(): Result<MyPageResponse> {
         delay(300)
         return runCatching {
-            MyPageInfoResponse(
+            MyPageResponse(
                 "한수아",
                 "HANSUAH",
                 "",
                 5,
                 listOf(
-                    MyPageInfoResponse.Photo(
-                        1,
-                        fakeImageUrl,
-                        "인생네컷",
-                        "2023.06.26"
-                    ),
-                    MyPageInfoResponse.Photo(
-                        2,
-                        fakeImageUrl,
-                        "인생네컷",
-                        "2023.06.26"
-                    ),
-                    MyPageInfoResponse.Photo(
-                        3,
-                        fakeImageUrl,
-                        "인생네컷",
-                        "2023.06.26"
-                    ),
-                    MyPageInfoResponse.Photo(
-                        4,
-                        fakeImageUrl,
-                        "인생네컷",
-                        "2023.06.26"
-                    ),
-                    MyPageInfoResponse.Photo(
-                        5,
-                        fakeImageUrl,
-                        "인생네컷",
-                        "2023.06.26"
-                    ),
-                    MyPageInfoResponse.Photo(
-                        5,
-                        fakeImageUrl,
-                        "인생네컷",
-                        "2023.06.26"
-                    ),
-                    MyPageInfoResponse.Photo(
-                        5,
-                        fakeImageUrl,
-                        "인생네컷",
-                        "2023.06.26"
-                    ),
-                    MyPageInfoResponse.Photo(
-                        5,
-                        fakeImageUrl,
-                        "인생네컷",
-                        "2023.06.26"
-                    ),
-                    MyPageInfoResponse.Photo(
-                        5,
-                        fakeImageUrl,
-                        "인생네컷",
-                        "2023.06.26"
-                    ),
-                    MyPageInfoResponse.Photo(
-                        5,
-                        fakeImageUrl,
-                        "인생네컷",
-                        "2023.06.26"
-                    ),
-                    MyPageInfoResponse.Photo(
-                        5,
-                        fakeImageUrl,
-                        "인생네컷",
-                        "2023.06.26"
-                    ),
-                    MyPageInfoResponse.Photo(
-                        5,
-                        fakeImageUrl,
-                        "인생네컷",
-                        "2023.06.26"
-                    ),
-                    MyPageInfoResponse.Photo(
-                        5,
-                        fakeImageUrl,
-                        "인생네컷",
-                        "2023.06.26"
-                    ),
-                    MyPageInfoResponse.Photo(
-                        5,
-                        fakeImageUrl,
-                        "인생네컷",
-                        "2023.06.26"
-                    )
+
                 )
             )
         }
