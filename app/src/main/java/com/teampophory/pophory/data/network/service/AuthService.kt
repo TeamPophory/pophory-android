bfc089b7a57c7142d475c21fe838be1cf8c1722d
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-04 19:10:07 +0900

    응답 데이터 모델 위치 수정

--- app/src/main/java/com/teampophory/pophory/data/network/service/AuthService.kt
+++ app/src/main/java/com/teampophory/pophory/data/network/service/AuthService.kt
@@ -1,6 +1,6 @@
 package com.teampophory.pophory.data.network.service
 
-import com.teampophory.pophory.data.network.model.AuthResponse
+import com.teampophory.pophory.data.network.model.auth.AuthResponse
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.PUT
