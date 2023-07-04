ee11e6741320f61d452ca97269fe758554395021
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-04 18:29:12 +0900

    Kakao Service 주입받을 수 있는 Bind module 생성

--- app/src/main/java/com/teampophory/pophory/config/di/OAuthModule.kt
+++ app/src/main/java/com/teampophory/pophory/config/di/OAuthModule.kt
@@ -1,11 +1,16 @@
 package com.teampophory.pophory.config.di
 
 import com.kakao.sdk.user.UserApiClient
+import com.teampophory.pophory.config.di.qualifier.Kakao
+import com.teampophory.pophory.feature.auth.social.KakaoAuthService
+import com.teampophory.pophory.feature.auth.social.OAuthService
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(ActivityComponent::class)
@@ -13,4 +18,13 @@ object OAuthModule {
     @Provides
     @ActivityScoped
     fun provideKakaoApiClient(): UserApiClient = UserApiClient.instance
+
+    @Module
+    @InstallIn(SingletonComponent::class)
+    interface Binder {
+        @ActivityScoped
+        @Binds
+        @Kakao
+        fun bindKakaoAuthService(service: KakaoAuthService): OAuthService
+    }
 }
