e3f125f7f13bb05349d44064ab222723da614b2e
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-05 23:38:29 +0900

    WebView 화면 추가 및 Navigation

---
+++ app/src/main/java/com/teampophory/pophory/feature/setting/webview/WebViewActivity.kt
@@ -0,0 +1,28 @@
+package com.teampophory.pophory.feature.setting.webview
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import com.teampophory.pophory.common.intent.stringExtra
+import com.teampophory.pophory.design.PophoryTheme
+
+class WebViewActivity : AppCompatActivity() {
+    private val url by stringExtra("")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PophoryTheme {
+                WebViewScreen(url = url.orEmpty(), onBack = { finish() })
+            }
+        }
+    }
+
+    companion object {
+        fun newIntent(context: Context, url: String) =
+            Intent(context, WebViewActivity::class.java).apply {
+                putExtra("url", url)
+            }
+    }
+}
--- app/src/main/java/com/teampophory/pophory/feature/setting/webview/WebViewScreen.kt
+++ app/src/main/java/com/teampophory/pophory/feature/setting/webview/WebViewScreen.kt
@@ -1,6 +1,7 @@
-package com.teampophory.pophory.feature.setting
+package com.teampophory.pophory.feature.setting.webview
 
-import androidx.compose.foundation.layout.Column
+import android.annotation.SuppressLint
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,40 +15,51 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
+import com.google.accompanist.web.AccompanistWebViewClient
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewNavigator
+import com.google.accompanist.web.rememberWebViewState
 import com.teampophory.pophory.R
 import com.teampophory.pophory.common.compose.DefaultPreview
 import com.teampophory.pophory.common.compose.bottomBorder
 import com.teampophory.pophory.design.PophoryTheme
-import com.teampophory.pophory.feature.setting.component.SettingItem
 
+private val webViewClient = AccompanistWebViewClient()
+
+@SuppressLint("SetJavaScriptEnabled")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SettingScreen(
-    onNavigateHome: () -> Unit = {},
-    onNavigateNotice: () -> Unit = {},
-    onNavigatePersonalTerms: () -> Unit = {},
-    onNavigateTerm: () -> Unit = {},
-    onLogout: () -> Unit = {},
-    onWithdrawal: () -> Unit = {},
+fun WebViewScreen(
+    url: String,
+    onBack: () -> Unit = {}
 ) {
+    val title by remember(webViewClient.state) { mutableStateOf(webViewClient.state.pageTitle) }
+    val state = rememberWebViewState(url)
+    val webViewNavigator = rememberWebViewNavigator()
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
                     Text(
-                        text = "설정",
+                        text = title.orEmpty(),
                         textAlign = TextAlign.Center,
                         modifier = Modifier.fillMaxWidth()
                     )
                 },
                 navigationIcon = {
-                    IconButton(onClick = onNavigateHome) {
+                    IconButton(onClick = {
+                        if (webViewNavigator.canGoBack) {
+                            webViewNavigator.navigateBack()
+                        } else {
+                            onBack()
+                        }
+                    }) {
                         Icon(
                             painter = painterResource(id = R.drawable.ic_chevron_left),
                             contentDescription = "Back To Home"
@@ -58,24 +70,28 @@ fun SettingScreen(
             )
         }
     ) {
-        Column(
-            modifier = Modifier
-                .padding(it)
-                .fillMaxSize()
-        ) {
-            SettingItem(title = "공지사항", onClick = onNavigateNotice)
-            SettingItem(title = "개인정보 처리방침", onClick = onNavigatePersonalTerms)
-            SettingItem(title = "이용약관", onClick = onNavigateTerm)
-            SettingItem(title = "로그아웃", onClick = onLogout)
-            SettingItem(title = "탈퇴하기", onClick = onWithdrawal)
+        WebView(
+            state = state,
+            modifier = Modifier.padding(it),
+            onCreated = { webview ->
+                webview.settings.javaScriptEnabled = true
+            },
+            client = webViewClient
+        )
+        BackHandler {
+            if (webViewNavigator.canGoBack) {
+                webViewNavigator.navigateBack()
+            } else {
+                onBack()
+            }
         }
     }
 }
 
 @DefaultPreview
 @Composable
-private fun SettingScreenPreview() {
+private fun WebViewScreenPreview() {
     PophoryTheme {
-        SettingScreen()
+        WebViewScreen("https://www.google.com")
     }
 }
