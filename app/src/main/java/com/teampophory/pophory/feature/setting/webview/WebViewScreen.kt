ea2740ee8a56684aaeac2f0eeebcb406df06a7c5
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-05 23:58:09 +0900

    WebView 로딩 구현 및 url 로드 구현 완료

--- app/src/main/java/com/teampophory/pophory/feature/setting/webview/WebViewScreen.kt
+++ app/src/main/java/com/teampophory/pophory/feature/setting/webview/WebViewScreen.kt
@@ -2,10 +2,14 @@ package com.teampophory.pophory.feature.setting.webview
 
 import android.annotation.SuppressLint
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,10 +19,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.web.AccompanistWebViewClient
 import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewNavigator
@@ -28,8 +34,6 @@ import com.teampophory.pophory.common.compose.DefaultPreview
 import com.teampophory.pophory.common.compose.bottomBorder
 import com.teampophory.pophory.design.PophoryTheme
 
-private val webViewClient = AccompanistWebViewClient()
-
 @SuppressLint("SetJavaScriptEnabled")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,9 +41,10 @@ fun WebViewScreen(
     url: String,
     onBack: () -> Unit = {}
 ) {
-    val title by remember(webViewClient.state) { mutableStateOf(webViewClient.state.pageTitle) }
     val state = rememberWebViewState(url)
+    val title by remember(state) { mutableStateOf(state.pageTitle) }
     val webViewNavigator = rememberWebViewNavigator()
+    val webViewClient = remember { AccompanistWebViewClient() }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -70,9 +75,26 @@ fun WebViewScreen(
             )
         }
     ) {
+        if (state.isLoading) {
+            Box(
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(40.dp),
+                    color = PophoryTheme.colors.primary,
+                    strokeWidth = 4.dp,
+                )
+            }
+        }
         WebView(
             state = state,
-            modifier = Modifier.padding(it),
+            modifier = Modifier
+                .padding(it)
+                .background(PophoryTheme.colors.error),
             onCreated = { webview ->
                 webview.settings.javaScriptEnabled = true
             },
