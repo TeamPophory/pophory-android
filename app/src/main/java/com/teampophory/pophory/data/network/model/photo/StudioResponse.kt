ce8eb4c1d2e47d23e6644ec3569ce25e7c3c7fd1
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-07 00:31:31 +0900

    스튜디오 fetch 로직에 사용할 데이터 모델 구축

---
+++ app/src/main/java/com/teampophory/pophory/data/network/model/photo/StudioResponse.kt
@@ -0,0 +1,30 @@
+package com.teampophory.pophory.data.network.model.photo
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+typealias StudioEntity = com.teampophory.pophory.data.model.photo.Studio
+
+@Serializable
+data class StudioResponse(
+    @SerialName("studios")
+    val studios: List<Studio>
+) {
+    @Serializable
+    data class Studio(
+        @SerialName("id")
+        val id: Long,
+        @SerialName("name")
+        val name: String,
+        @SerialName("imageUrl")
+        val imageUrl: String? = null
+    ) {
+        fun toStudio() = StudioEntity(
+            id = id,
+            name = name,
+            imageUrl = imageUrl
+        )
+    }
+
+    fun toStudios() = studios.map { it.toStudio() }
+}
