ce47f92f0785a1c99655fd8ef29c5c62f089b275
Jihyeon An <jh011007@naver.com>
2023-07-06 23:53:52 +0900

    [feat/signup] : 앨범 커버 적용

--- app/src/main/java/com/teampophory/pophory/feature/signup/SignUpThirdFragment.kt
+++ app/src/main/java/com/teampophory/pophory/feature/signup/SignUpThirdFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+
+import com.teampophory.pophory.R
 import com.teampophory.pophory.common.view.viewBinding
 import com.teampophory.pophory.databinding.FragmentSignUpThirdBinding
 
@@ -21,7 +23,21 @@ class SignUpThirdFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setAlbumCoverImage()
         selectAlbumCover()
+    }
+
+    private fun setAlbumCoverImage() {
+        //기본 앨범 커버 이미지
+        binding.ivAlbumCover.setImageResource(R.drawable.ic_album_cover_friends)
+
+    }
+
+    private fun setAlbumSelectState(number: Int){
+        binding.ivAlbumSelect1.isVisible = number == 1
+        binding.ivAlbumSelect2.isVisible = number == 2
+        binding.ivAlbumSelect3.isVisible = number == 3
+        binding.ivAlbumSelect4.isVisible = number == 4
     }
 
     private fun selectAlbumCover() {
@@ -29,29 +45,25 @@ class SignUpThirdFragment : Fragment() {
         binding.ivAlbumSelect2.isVisible = false
         binding.ivAlbumSelect3.isVisible = false
         binding.ivAlbumSelect4.isVisible = false
+
         binding.ivAlbumCover1.setOnClickListener {
-            binding.ivAlbumSelect1.isVisible = true
-            binding.ivAlbumSelect2.isVisible = false
-            binding.ivAlbumSelect3.isVisible = false
-            binding.ivAlbumSelect4.isVisible = false
+            setAlbumSelectState(1)
+            binding.ivAlbumCover.setImageResource(R.drawable.ic_album_cover_friends)
         }
         binding.ivAlbumCover2.setOnClickListener {
-            binding.ivAlbumSelect1.isVisible = false
-            binding.ivAlbumSelect2.isVisible = true
-            binding.ivAlbumSelect3.isVisible = false
-            binding.ivAlbumSelect4.isVisible = false
+            setAlbumSelectState(2)
+
+            binding.ivAlbumCover.setImageResource(R.drawable.ic_album_cover_love)
         }
         binding.ivAlbumCover3.setOnClickListener {
-            binding.ivAlbumSelect1.isVisible = false
-            binding.ivAlbumSelect2.isVisible = false
-            binding.ivAlbumSelect3.isVisible = true
-            binding.ivAlbumSelect4.isVisible = false
+            setAlbumSelectState(3)
+
+            binding.ivAlbumCover.setImageResource(R.drawable.ic_album_cover_myalbum)
         }
         binding.ivAlbumCover4.setOnClickListener {
-            binding.ivAlbumSelect1.isVisible = false
-            binding.ivAlbumSelect2.isVisible = false
-            binding.ivAlbumSelect3.isVisible = false
-            binding.ivAlbumSelect4.isVisible = true
+            setAlbumSelectState(4)
+
+            binding.ivAlbumCover.setImageResource(R.drawable.ic_album_cover_collectbook)
         }
     }
 }
