360d5eeef5f170174dcd0aa9b1f896398f3149f9
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-06 10:39:10 +0900

    Background 이미지 넣기

--- app/src/main/java/com/teampophory/pophory/feature/home/photo/AddPhotoActivity.kt
+++ app/src/main/java/com/teampophory/pophory/feature/home/photo/AddPhotoActivity.kt
@@ -1,6 +1,14 @@
 package com.teampophory.pophory.feature.home.photo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teampophory.pophory.R
+import com.teampophory.pophory.common.activity.BindingActivity
+import com.teampophory.pophory.databinding.ActivityAddPhotoBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class AddPhotoActivity: AppCompatActivity() {
+@AndroidEntryPoint
+class AddPhotoActivity : BindingActivity<ActivityAddPhotoBinding>(R.layout.activity_add_photo) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
