c68dbd4300c7a8106c3022c0e6a8bdf8513fe172
곽의진 <yuijin0909@gmail.com>
2023-07-07 01:17:48 +0900

    [feat/album_detail]: rebase and sync develop

--- app/src/main/java/com/teampophory/pophory/bottomsheet/ModalBottomSheet.kt
+++ app/src/main/java/com/teampophory/pophory/bottomsheet/ModalBottomSheet.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.teampophory.pophory.databinding.ModalBottomSheetContentBinding
 
--- app/src/main/java/com/teampophory/pophory/feature/home/store/StoreFragment.kt
+++ app/src/main/java/com/teampophory/pophory/feature/home/store/StoreFragment.kt
@@ -16,6 +16,7 @@ import com.teampophory.pophory.common.primitive.textAppearance
 import com.teampophory.pophory.common.view.viewBinding
 import com.teampophory.pophory.databinding.FragmentStoreBinding
 import com.teampophory.pophory.feature.album.list.AlbumListActivity
+import com.teampophory.pophory.feature.home.store.apdater.OnPageChangedListener
 import com.teampophory.pophory.feature.home.store.apdater.StoreAdapter
 import com.teampophory.pophory.feature.home.store.model.AlbumItem
 import dagger.hilt.android.AndroidEntryPoint
