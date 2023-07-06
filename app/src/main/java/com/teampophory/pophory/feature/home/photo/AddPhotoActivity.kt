15cf511c740177401b7c45e0ec915c2d22acc08b
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-06 22:46:03 +0900

    일정 변경 범위 설정

--- app/src/main/java/com/teampophory/pophory/feature/home/photo/AddPhotoActivity.kt
+++ app/src/main/java/com/teampophory/pophory/feature/home/photo/AddPhotoActivity.kt
@@ -1,11 +1,14 @@
 package com.teampophory.pophory.feature.home.photo
 
 import android.os.Bundle
+import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.teampophory.pophory.R
 import com.teampophory.pophory.common.activity.BindingActivity
+import com.teampophory.pophory.common.time.systemNow
 import com.teampophory.pophory.databinding.ActivityAddPhotoBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.datetime.Instant
 
 @AndroidEntryPoint
 class AddPhotoActivity : BindingActivity<ActivityAddPhotoBinding>(R.layout.activity_add_photo) {
@@ -15,6 +18,12 @@ class AddPhotoActivity : BindingActivity<ActivityAddPhotoBinding>(R.layout.activ
             MaterialDatePicker.Builder
                 .datePicker()
                 .setTheme(R.style.ThemeOverlay_Pophory_MaterialCalendar)
+                .setInputMode(MaterialDatePicker.INPUT_MODE_CALENDAR)
+                .setCalendarConstraints(
+                    CalendarConstraints.Builder()
+                        .setEnd(Instant.systemNow().toEpochMilliseconds())
+                        .build()
+                )
                 .build()
                 .show(supportFragmentManager, "datePicker")
         }
