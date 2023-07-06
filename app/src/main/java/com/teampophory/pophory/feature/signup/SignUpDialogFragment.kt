8ad31c503ef5023938ac80fd23bc642097800623
Jihyeon An <jh011007@naver.com>
2023-07-06 22:41:53 +0900

    [feat/dialogFragment] : requestFeature 삭제

--- app/src/main/java/com/teampophory/pophory/feature/signup/SignUpDialogFragment.kt
+++ app/src/main/java/com/teampophory/pophory/feature/signup/SignUpDialogFragment.kt
@@ -44,7 +44,6 @@ class SignUpDialogFragment : DialogFragment() {
         }
         dialog?.window?.run {
             setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-            requestFeature(Window.FEATURE_NO_TITLE)
         }
     }
 
