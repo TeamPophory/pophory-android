c40f7b7f23e0ccc70865935ae0abf7754b7c40d5
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-07 08:36:15 +0900

    데이터 받아오고 이미지 세팅하는 로직 추가

--- app/src/main/java/com/teampophory/pophory/feature/home/photo/AddPhotoActivity.kt
+++ app/src/main/java/com/teampophory/pophory/feature/home/photo/AddPhotoActivity.kt
@@ -1,14 +1,21 @@
 package com.teampophory.pophory.feature.home.photo
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import coil.load
 import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.DateValidatorPointBackward
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.teampophory.pophory.R
 import com.teampophory.pophory.common.activity.BindingActivity
+import com.teampophory.pophory.common.image.getImageSize
+import com.teampophory.pophory.common.intent.intExtra
+import com.teampophory.pophory.common.intent.parcelableExtra
 import com.teampophory.pophory.common.time.systemNow
 import com.teampophory.pophory.databinding.ActivityAddPhotoBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,15 +30,39 @@ import java.util.TimeZone
 @AndroidEntryPoint
 class AddPhotoActivity : BindingActivity<ActivityAddPhotoBinding>(R.layout.activity_add_photo) {
     private val viewModel: AddPhotoViewModel by viewModels()
+    private val imageUri by parcelableExtra<Uri>()
+    private val albumCoverId by intExtra()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        loadImage()
         initView()
         subscribeEvent()
         collectState()
     }
 
+    private fun loadImage() {
+        val imageSize = imageUri?.getImageSize()
+        imageSize?.let {
+            if (it.width >= it.height) {
+                binding.imgBackground.load(R.drawable.img_background_width)
+                binding.imgHorizontal.load(imageUri) {
+                    crossfade(true)
+                }
+                return
+            }
+            binding.imgBackground.load(R.drawable.img_background_height)
+            binding.imgVertical.load(imageUri) {
+                crossfade(true)
+            }
+        }
+    }
+
     private fun initView() {
+        binding.toolbarAddPhoto.btnBack.setOnClickListener {
+            finish()
+        }
+        binding.toolbarAddPhoto.txtToolbarTitle.text = "사진 추가"
         binding.layoutDate.setOnClickListener {
             viewModel.onCreatedAtPressed()
         }
@@ -83,5 +114,14 @@ class AddPhotoActivity : BindingActivity<ActivityAddPhotoBinding>(R.layout.activ
                         Locale.getDefault()
                     ).format(Date(it))
             }.launchIn(lifecycleScope)
+    }
+
+    companion object {
+        @JvmStatic
+        fun getIntent(context: Context, imageUri: Uri, albumCoverId: Int) =
+            Intent(context, AddPhotoActivity::class.java).apply {
+                putExtra("imageUri", imageUri)
+                putExtra("albumCoverId", albumCoverId)
+            }
     }
 }
