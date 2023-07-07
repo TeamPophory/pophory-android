da08ac8e28e53faab3400755813ca689c9e6d05a
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-07 09:30:02 +0900

    Uri에서 Image 가져오는 Utils 생성

--- core/common/src/main/java/com/teampophory/pophory/common/image/UriExt.kt
+++ core/common/src/main/java/com/teampophory/pophory/common/image/UriExt.kt
@@ -1,9 +1,14 @@
 package com.teampophory.pophory.common.image
 
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.content.Context
 import android.graphics.BitmapFactory
 import android.net.Uri
+import android.provider.MediaStore
 import android.util.Size
 import androidx.core.net.toFile
+import java.io.File
 
 fun Uri.getImageSize(): Size {
     val options = BitmapFactory.Options().apply {
@@ -11,4 +16,28 @@ fun Uri.getImageSize(): Size {
     }
     BitmapFactory.decodeFile(toFile().path, options)
     return Size(options.outWidth, options.outHeight)
+}
+
+fun Uri?.toImageContent(context: Context): Uri? {
+    return if (this?.scheme?.equals(ContentResolver.SCHEME_CONTENT) == true) {
+        // 기본 갤러리 앱 등에서 받은 content:/// Uri를 file:/// 스키마의 Uri로 변경
+        try {
+            toFileUri(context)
+        } catch (e: Exception) {
+            this
+        }
+    } else {
+        this
+    }
+}
+
+@SuppressLint("Range")
+private fun Uri?.toFileUri(context: Context): Uri? {
+    return context.contentResolver
+        .query(this!!, null, null, null, null)?.use { cursor ->
+            if (cursor.moveToFirst()) {
+                return@use Uri.fromFile(File(cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA))))
+            }
+            return@use null
+        }
 }
