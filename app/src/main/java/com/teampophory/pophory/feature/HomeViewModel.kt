49507e39ff47321baa8b4b56c1002cfb2f76ecc5
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-08 13:34:54 +0900

    센트리 유저 설정

--- app/src/main/java/com/teampophory/pophory/feature/HomeViewModel.kt
+++ app/src/main/java/com/teampophory/pophory/feature/HomeViewModel.kt
@@ -2,14 +2,30 @@ package com.teampophory.pophory.feature
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.teampophory.pophory.domain.ConfigureMeUseCase
 import com.teampophory.pophory.feature.home.store.model.AlbumItem
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val fetchMeUseCase: ConfigureMeUseCase
+) : ViewModel() {
     private val _currentAlbum = MutableStateFlow<AlbumItem?>(null)
     val currentAlbum: StateFlow<AlbumItem?> get() = _currentAlbum
+
+    init {
+        viewModelScope.launch {
+            fetchMeUseCase()
+                .onSuccess {
+
+                }.onFailure(Timber::e)
+        }
+    }
 
     fun onUpdateAlbum(album: AlbumItem?) {
         viewModelScope.launch {
