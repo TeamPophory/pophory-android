9f1298feb2527c940a093a9ffd7720379b112f80
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-11 14:26:50 +0900

    Authenticator 제작

--- app/src/main/java/com/teampophory/pophory/data/network/service/RefreshApi.kt
+++ app/src/main/java/com/teampophory/pophory/data/network/service/RefreshApi.kt
@@ -1,9 +1,11 @@
 package com.teampophory.pophory.data.network.service
 
 import com.teampophory.pophory.data.model.auth.Token
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface RefreshApi {
+    // Header에 어떤 key, value가 들어가야하는 지 미지수
     @POST("api/v1/auth/token")
-    suspend fun refreshToken(): Token
+    suspend fun refreshToken(@Header("") token: String): Token
 }
