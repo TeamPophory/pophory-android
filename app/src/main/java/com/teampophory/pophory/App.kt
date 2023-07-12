d0528420f67975e30aa003aa756ee49f63bf585e
Seo jaewon <52442547+librarywon@users.noreply.github.com>
2023-07-12 19:35:59 +0900

    [feature/rotate_block]: add rotate block (#143)

--- app/src/main/java/com/teampophory/pophory/App.kt
+++ app/src/main/java/com/teampophory/pophory/App.kt
@@ -1,6 +1,9 @@
 package com.teampophory.pophory
 
+import android.app.Activity
 import android.app.Application
+import android.content.pm.ActivityInfo
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
@@ -10,5 +13,20 @@ class App : Application() {
         super.onCreate()
         FlipperInitializer.execute(this)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        registerActivityLifecycleCallbacks(getActivityLifeCycleCallback())
     }
+
+    private fun getActivityLifeCycleCallback() =
+        object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            }
+
+            override fun onActivityStarted(activity: Activity) = Unit
+            override fun onActivityResumed(activity: Activity) = Unit
+            override fun onActivityPaused(activity: Activity) = Unit
+            override fun onActivityStopped(activity: Activity) = Unit
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
+            override fun onActivityDestroyed(activity: Activity) = Unit
+        }
 }
