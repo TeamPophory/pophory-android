6acf41e49dc65b682c095c151d9315bc8036da56
Jihyeon An <jh011007@naver.com>
2023-07-14 13:48:30 +0900

    [feat/refactor_signup] : navigation 화면 이동

--- app/src/main/java/com/teampophory/pophory/feature/signup/SignUpActivity.kt
+++ app/src/main/java/com/teampophory/pophory/feature/signup/SignUpActivity.kt
@@ -23,9 +23,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class SignUpActivity : AppCompatActivity() {
 
     private val binding by viewBinding(ActivitySignUpBinding::inflate)
-    //TODO 수정 필요
-    private val callback = object : OnBackPressedCallback(true) {
-        override fun handleOnBackPressed() {
 
         }
     }
@@ -34,22 +31,16 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        observeEvent()
+
         //TODO 수정 필요
         binding.btnNext.isEnabled = true
-
-//        val finalHost = NavHostFragment.create(R.navigation.nav_signup_graph)
-//        supportFragmentManager.beginTransaction()
-//            .replace(R.id.nav_host_fragment, finalHost)
-//            .setPrimaryNavigationFragment(finalHost) // this is the equivalent to app:defaultNavHost="true"
-//            .commit()
 
         //다음 버튼
         setOnClickNextButton()
         //툴바 뒤로 가기 버튼
         setOnToolbarBackPressed()
-        //기기 뒤로 가기 버튼
-        //TODO 수정 필요
-        this.onBackPressedDispatcher.addCallback(this, callback)
+        //TODO 기기 뒤로 가기 버튼
     }
 
     private fun setOnToolbarBackPressed() {
@@ -63,34 +54,41 @@ class SignUpActivity : AppCompatActivity() {
                     val intent = Intent(this, OnBoardingActivity::class.java)
                     startActivity(intent)
                 }
-                SIGN_UP_SECOND_FRAGMENT -> {
-                    navController.navigate(R.id.action_signUpSecondFragment_to_signUpFirstFragment)
-                }
-                SIGN_UP_THIRD_FRAGMENT -> {
-                    navController.navigate(R.id.action_signUpThirdFragment_to_signUpSecondFragment)
-                }
+
+                SIGN_UP_SECOND_FRAGMENT -> navController.popBackStack()
+
+                SIGN_UP_THIRD_FRAGMENT -> navController.popBackStack()
             }
         }
     }
 
     private fun setOnClickNextButton() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.findNavController()
 
         binding.btnNext.setOnClickListener {
             val currentFragment = navController.currentDestination?.label
             when (currentFragment) {
-                SIGN_UP_FIRST_FRAGMENT -> {
-                    navController.navigate(R.id.action_signUpFirstFragment_to_signUpSecondFragment)
-                }
-                SIGN_UP_SECOND_FRAGMENT -> {
-                    navController.navigate(R.id.action_signUpSecondFragment_to_signUpThirdFragment)
-                }
+                SIGN_UP_FIRST_FRAGMENT -> navigateToFragment(1)
+                SIGN_UP_SECOND_FRAGMENT -> navigateToFragment(2)
                 SIGN_UP_THIRD_FRAGMENT -> {
                     val intent = Intent(this, StartPophoryActivity::class.java)
                     startActivity(intent)
                 }
             }
+        }
+    }
+
+    private fun navigateToFragment(start: Int) {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+
+        when (start) {
+            1 -> navController.navigate(R.id.action_signUpFirstFragment_to_signUpSecondFragment)
+            2 -> navController.navigate(R.id.action_signUpSecondFragment_to_signUpThirdFragment)
         }
     }
 
@@ -100,76 +98,26 @@ class SignUpActivity : AppCompatActivity() {
         const val SIGN_UP_THIRD_FRAGMENT = "SignUpThirdFragment"
     }
 
+    private fun observeEvent() {
 
+        viewModel.signUpResult.observe(this) {
+            startActivity(StartPophoryActivity.getIntent(this))
+        }
 
-//    private fun observeEvent() {
-//        viewModel.signUpResult.observe(this) {
-//            startActivity(StartPophoryActivity.getIntent(this))
-//        }
-//
-//        viewModel.nicknameCheckResult.observe(this) {
-////            if (!it.isDuplicated) {
-////                val nextPosition = currentPosition + 1
-////                binding.viewpager.currentItem = nextPosition
-////            } else {
-////                supportFragmentManager.commit(allowStateLoss = true) {
-////                    add(SignUpDialogFragment(), SignUpDialogFragment::class.java.simpleName)
-////                }
-////            }
-//        }
-//        viewModel.buttonState.observe(this) {
-//            binding.btnNext.isEnabled = it
-//        }
-//    }
+        viewModel.nicknameCheckResult.observe(this) {
+            if (!it.isDuplicated) {
+                navigateToFragment(2)
+            } else {
+                supportFragmentManager.commit(allowStateLoss = true) {
+                    add(SignUpDialogFragment(), SignUpDialogFragment::class.java.simpleName)
+                }
+            }
+        }
+        viewModel.buttonState.observe(this) {
+            binding.btnNext.isEnabled = it
+        }
+    }
 
-//    private fun setOnBackPressed() {
-//        binding.btnBack.setOnClickListener {
-////            when (currentPosition) {
-////                0 -> {
-////                    val intent = Intent(this, OnBoardingActivity::class.java)
-////                    startActivity(intent)
-////                    finish()
-////                }
-////
-////                1 -> {
-////                    binding.viewpager.currentItem = 0
-////                }
-////
-////                2 -> {
-////                    binding.viewpager.currentItem = 1
-////                }
-////            }
-//        }
-//    }
-
-
-//    private fun setOnClickNextButton() {
-//        binding.btnNext.setOnClickListener {
-//            it.findNavController().navigate(R.id.action_signUpFirstFragment_to_signUpSecondFragment)
-//
-////            when (currentPosition) {
-////                0 -> binding.viewpager.currentItem = currentPosition + 1
-////                1 -> viewModel.nicknameCheck()
-////                2 -> viewModel.signUp()
-////            }
-//        }
-//    }
-//
-//
-//    private val pageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
-//        override fun onPageScrolled(
-//            position: Int,
-//            positionOffset: Float,
-//            positionOffsetPixels: Int
-//        ) {
-//            super.onPageScrolled(position, positionOffset, positionOffsetPixels)
-////            with(binding) {
-////                tabFirst.isSelected = position == 0
-////                tabSecond.isSelected = position == 1
-////                tabThird.isSelected = position == 2
-////            }
-//
-//        }
 //
 //        override fun onPageSelected(position: Int) {
 //            super.onPageSelected(position)
@@ -177,13 +125,5 @@ class SignUpActivity : AppCompatActivity() {
 //            binding.btnNext.text = if (position == 2) "완료하기" else "다음으로 넘어가기"
 //            binding.btnNext.isEnabled = position == 2
 //        }
-//    }
-//
-//    private fun setViewPager() {
-////        binding.viewpager.apply {
-////            adapter = SignUpViewPagerAdapter(this@SignUpActivity)
-////            isUserInputEnabled = false
-////        }
-////        binding.viewpager.registerOnPageChangeCallback(pageChangeCallback)
 //    }
 }
