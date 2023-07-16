0933d87b2173f07f936029ac13bca5b9a24a89dd
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-16 10:01:10 +0900

    Add function to caculate allocated bytes size of bitmap

--- core/common/src/main/java/com/teampophory/pophory/common/image/UriExt.kt
+++ core/common/src/main/java/com/teampophory/pophory/common/image/UriExt.kt
@@ -19,3 +19,18 @@ fun Uri.getImageSize(context: Context): Size {
     }
     return Size(options.outWidth, options.outHeight)
 }
+
+fun Uri.getAllocatedByte(context: Context): Long {
+    val options = BitmapFactory.Options().apply {
+        inJustDecodeBounds = true
+    }
+
+    if (scheme.equals("file")) {
+        BitmapFactory.decodeFile(path, options)
+    } else {
+        context.contentResolver.openInputStream(this)?.use {
+            BitmapFactory.decodeStream(it, null, options)
+        }
+    }
+    return options.outWidth * options.outHeight * 4L
+}
