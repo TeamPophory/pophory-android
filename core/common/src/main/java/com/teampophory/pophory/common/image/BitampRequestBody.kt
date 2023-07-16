1bd52efe6339362ee03e2036b899c97e71882e12
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-16 10:32:09 +0900

    Add BitmapRequestBody

--- core/common/src/main/java/com/teampophory/pophory/common/image/BitampRequestBody.kt
+++ core/common/src/main/java/com/teampophory/pophory/common/image/BitampRequestBody.kt
@@ -1,2 +1,15 @@
 package com.teampophory.pophory.common.image
 
+import android.graphics.Bitmap
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody
+import okio.BufferedSink
+
+class BitmapRequestBody(private val bitmap: Bitmap) : RequestBody() {
+    override fun contentType(): MediaType = "image/jpeg".toMediaType()
+
+    override fun writeTo(sink: BufferedSink) {
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, sink.outputStream())
+    }
+}
