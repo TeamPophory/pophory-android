f56a80c5486e66901d2c3e53c3337e23b5ae403a
librarywon <librarywon@naver.com>
2023-07-18 03:21:25 +0900

    [feature/share]: photo albumId Int to Long

--- app/src/main/java/com/teampophory/pophory/data/repository/fake/FakePhotoRepository.kt
+++ app/src/main/java/com/teampophory/pophory/data/repository/fake/FakePhotoRepository.kt
@@ -11,7 +11,7 @@ class FakePhotoRepository : PhotoRepository {
     private val fakeImageUrl =
         "https://images.unsplash.com/photo-1687023956422-117d5c47029d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3432&q=80"
 
-    override suspend fun getPhotos(id: Int): Result<PhotoListResponse> {
+    override suspend fun getPhotos(id: Long): Result<PhotoListResponse> {
         delay(300)
         return runCatching {
             PhotoListResponse(
@@ -74,7 +74,7 @@ class FakePhotoRepository : PhotoRepository {
     }
 
     override suspend fun addPhotoToPophory(
-        albumId: Int,
+        albumId: Long,
         takenAt: String,
         studioId: Long,
         fileName: String,
--- app/src/main/java/com/teampophory/pophory/domain/repository/photo/PhotoRepository.kt
+++ app/src/main/java/com/teampophory/pophory/domain/repository/photo/PhotoRepository.kt
@@ -6,14 +6,14 @@ import com.teampophory.pophory.data.network.model.album.PhotoListResponse
 import com.teampophory.pophory.domain.model.S3Image
 
 interface PhotoRepository {
-    suspend fun getPhotos(id: Int): Result<PhotoListResponse>
+    suspend fun getPhotos(id: Long): Result<PhotoListResponse>
 
     suspend fun deletePhoto(photoId: Long): Result<Unit>
 
     suspend fun getStudios(): Result<List<Studio>>
 
     suspend fun addPhotoToPophory(
-        albumId: Int,
+        albumId: Long,
         takenAt: String,
         studioId: Long,
         fileName: String,
