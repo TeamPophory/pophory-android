4bfb6aa8400114acdf46024981b1415604daf687
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-17 11:49:09 +0900

    Add calculate allocatedByte logic

--- core/common/src/main/java/com/teampophory/pophory/common/image/UriExt.kt
+++ core/common/src/main/java/com/teampophory/pophory/common/image/UriExt.kt
@@ -3,6 +3,7 @@ package com.teampophory.pophory.common.image
 import android.content.Context
 import android.graphics.BitmapFactory
 import android.net.Uri
+import android.provider.MediaStore
 import android.util.Size
 
 fun Uri.getImageSize(context: Context): Size {
@@ -33,4 +34,17 @@ fun Uri.getAllocatedByte(context: Context): Long {
         }
     }
     return options.outWidth * options.outHeight * 4L
+}
+
+fun Uri.getAllocatedBytes(context: Context) = context.contentResolver.query(
+    this,
+    arrayOf(MediaStore.Images.Media.SIZE, MediaStore.Images.Media.DISPLAY_NAME),
+    null,
+    null,
+    null
+)?.use { cursor ->
+    if (cursor.moveToFirst()) {
+        return@use cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.SIZE))
+    }
+    return@use 0L
 }
