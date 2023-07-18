59a7bb28c23582c68e3fd8faec7ee82bede3bf66
Seo jaewon <52442547+librarywon@users.noreply.github.com>
2023-07-19 04:59:50 +0900

    [Feature/story] 포릿 이야기 엑티비티, 웹뷰 생성 (#187)
    * [feature/story]: feat 포릿 스토리 뷰 구성
    
    * [feature/share]: refactor to feature/story
    
    * [feature/story]: setting WebView
    
    * [feature/story]: feat Intent
    
    * [feature/story]: remove this
    
    * [feature/story]: chore actionbar size
    
    * [feature/story]: chore PR 대응
    
    * [fix/mypage]: image가 중복되어 레이아웃이 뭉개진 issue 해결
    
    * [fix/mypage]: backgrond image 이미지 변경
    
    * [fix/mypage]: UI background_profile height edit
    
    * [fix/album_detail]: 서버 값 수정 대응
    
    * [fix/store]: UI 디자이너 피드백 대응

---
+++ app/src/main/java/com/teampophory/pophory/feature/story/StoryActivity.kt
@@ -0,0 +1,44 @@
+package com.teampophory.pophory.feature.story
+
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebSettings
+import androidx.appcompat.app.AppCompatActivity
+import com.teampophory.pophory.common.view.viewBinding
+import com.teampophory.pophory.databinding.ActivityStoryBinding
+
+class StoryActivity : AppCompatActivity() {
+    private val binding: ActivityStoryBinding by viewBinding(ActivityStoryBinding::inflate)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setOnClickListener()
+        initWebView()
+    }
+
+    private fun initWebView() {
+        binding.wvStory.settings.apply {
+            javaScriptEnabled = true // 자바스크립트 허용
+            javaScriptCanOpenWindowsAutomatically = false // 자바스크립트 새창 띄우기 허용
+            loadWithOverviewMode = true // html의 컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
+            cacheMode = WebSettings.LOAD_NO_CACHE // 브라우저 캐쉬 허용
+
+            /**
+             * This request has been blocked; the content must be served over HTTPS
+             * https 에서 이미지가 표시 안되는 오류를 해결하기 위한 처리
+             */
+            mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        }
+        binding.wvStory.loadUrl(url)
+    }
+    private fun setOnClickListener() {
+        binding.ivStoryBack.setOnClickListener {
+            finish()
+        }
+    }
+
+    companion object {
+        val url = "https://pophoryofficial.wixsite.com/pophory/porit-story"
+    }
+}
