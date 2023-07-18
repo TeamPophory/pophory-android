e8f8c7902a3f06abdf3dcc53514fdcc8739064af
librarywon <librarywon@naver.com>
2023-07-19 03:20:59 +0900

    [feature/story]: remove this

--- app/src/main/java/com/teampophory/pophory/feature/story/StoryActivity.kt
+++ app/src/main/java/com/teampophory/pophory/feature/story/StoryActivity.kt
@@ -19,17 +19,16 @@ class StoryActivity : AppCompatActivity() {
 
     private fun initWebView() {
         binding.wvStory.settings.apply {
-            this.javaScriptEnabled = true // 자바스크립트 허용
-            this.javaScriptCanOpenWindowsAutomatically = false // 자바스크립트 새창 띄우기 허용
-            this.loadWithOverviewMode = true // html의 컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
-            this.cacheMode = WebSettings.LOAD_NO_CACHE // 브라우저 캐쉬 허용
+            javaScriptEnabled = true // 자바스크립트 허용
+            javaScriptCanOpenWindowsAutomatically = false // 자바스크립트 새창 띄우기 허용
+            loadWithOverviewMode = true // html의 컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
+            cacheMode = WebSettings.LOAD_NO_CACHE // 브라우저 캐쉬 허용
+
             /**
              * This request has been blocked; the content must be served over HTTPS
              * https 에서 이미지가 표시 안되는 오류를 해결하기 위한 처리
              */
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                this.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-            }
+            mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
         }
         binding.wvStory.loadUrl(url)
     }
