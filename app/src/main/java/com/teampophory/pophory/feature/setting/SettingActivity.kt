f1bc33bcc6b2bf3ce7bc3e63c4e5d4f60617a0e0
KwakEuiJin <93872496+KwakEuiJin@users.noreply.github.com>
2023-07-19 18:27:11 +0900

    [feature/change_url]: url change (#193)

--- app/src/main/java/com/teampophory/pophory/feature/setting/SettingActivity.kt
+++ app/src/main/java/com/teampophory/pophory/feature/setting/SettingActivity.kt
@@ -51,7 +51,7 @@ class SettingActivity : AppCompatActivity() {
                         startActivity(
                             WebViewActivity.newIntent(
                                 this,
-                                "https://pophoryofficial.wixsite.com/pophory/gaeinjeongbo-ceoribangcim/%E2%80%8B%EA%B0%9C%E[…]%EC%A0%95%EB%B3%B4-%EC%B2%98%EB%A6%AC%EB%B0%A9%EC%B9%A8"
+                                "https://pophoryofficial.wixsite.com/pophory/%EC%A0%95%EC%B1%85#policy2"
                             )
                         )
                     },
@@ -59,7 +59,7 @@ class SettingActivity : AppCompatActivity() {
                         startActivity(
                             WebViewActivity.newIntent(
                                 this,
-                                "https://pophoryofficial.wixsite.com/pophory/copy-of-gaeinjeongbo-ceoribangcim/%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%9D%B4%EC%9A%A9-%EC%95%BD%EA%B4%80"
+                                "https://pophoryofficial.wixsite.com/pophory/%EC%A0%95%EC%B1%85#policy1"
                             )
                         )
                     },
