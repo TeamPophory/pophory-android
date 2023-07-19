53337a36394eeda19b71ea7763791190486664a1
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-07-20 00:56:24 +0900

    공유 API 연결하기

---
+++ app/src/main/java/com/teampophory/pophory/data/network/service/ShareService.kt
@@ -0,0 +1,19 @@
+package com.teampophory.pophory.data.network.service
+
+import com.teampophory.pophory.data.network.model.share.SharePhotoResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface ShareService {
+    @GET("api/v2/share/{shareId}")
+    suspend fun getPhotoInfo(
+        @Path("shareId") shareId: String
+    ): SharePhotoResponse
+
+    @POST("api/v2/share/photo/{photoId}")
+    suspend fun acceptShare(
+        @Path("photoId") photoId: Long
+    ): Response<Unit>
+}
