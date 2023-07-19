74caf3f1607d1d261fdc7f7a23b68d73bea44f5f
librarywon <librarywon@naver.com>
2023-07-19 23:27:18 +0900

    [feature/signUp-image]: 앨범 커버 util수정

--- app/src/main/java/com/teampophory/pophory/util/AlbumCoverUtils.kt
+++ app/src/main/java/com/teampophory/pophory/util/AlbumCoverUtils.kt
@@ -3,9 +3,13 @@ package com.teampophory.pophory.util
 import com.teampophory.pophory.R
 
 fun Int.toCoverDrawable() = when (this) {
-    1 -> R.drawable.ic_album_cover_friends
-    2 -> R.drawable.ic_album_cover_love
-    3 -> R.drawable.ic_album_cover_myalbum
-    4 -> R.drawable.ic_album_cover_collectbook
+    1 -> R.drawable.ic_album_cover_friends_1
+    2 -> R.drawable.ic_album_cover_friends_2
+    3 -> R.drawable.ic_album_cover_love_1
+    4 -> R.drawable.ic_album_cover_love_2
+    5 -> R.drawable.ic_album_cover_me_1
+    6 -> R.drawable.ic_album_cover_me_2
+    7 -> R.drawable.ic_album_cover_family_1
+    8 -> R.drawable.ic_album_cover_family_2
     else -> throw IllegalArgumentException("Invalid album cover id $this")
 }
