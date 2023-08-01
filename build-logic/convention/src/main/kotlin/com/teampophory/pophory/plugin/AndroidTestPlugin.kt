609f37d24941a2206d07eb9610dd3b343e7c7108
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-08-02 00:43:50 +0900

    android test 의존성 전부 plugin으로 대체

--- build-logic/convention/src/main/kotlin/com/teampophory/pophory/plugin/AndroidTestPlugin.kt
+++ build-logic/convention/src/main/kotlin/com/teampophory/pophory/plugin/AndroidTestPlugin.kt
@@ -32,7 +32,6 @@ class AndroidTestPlugin: Plugin<Project> {
 
         dependencies {
             "testImplementation"(libs.findLibrary("junit").get())
-            "testImplementation"(libs.findLibrary("espresso").get())
             "debugImplementation"(libs.findLibrary("truth").get())
             "testImplementation"(libs.findLibrary("robolectric").get())
             "androidTestImplementation"(libs.findBundle("androidx.android.test").get())
