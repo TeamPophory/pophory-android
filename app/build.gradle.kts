2cf3f692c27f3cdbe54fd5b4907107d51011fb7c
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-08-09 22:07:51 +0900

    bottomnavigation 모듈 참조 방식 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
 }
 
 dependencies {
-    implementation(project(":bottomnavigation"))
+    implementation(projects.bottomnavigation)
 
     // domain
     implementation(projects.domain.auth)
