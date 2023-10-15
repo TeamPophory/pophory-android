5d5cfcb9fff7e0d45ec1b5c72a5f63da36905133
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-10-16 01:04:53 +0900

    remove kapt and databinding configuration

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -7,10 +7,6 @@ plugins {
 android {
     namespace = "com.teampophory.pophory.common"
 
-    buildFeatures {
-        dataBinding = true
-    }
-
     defaultConfig {
         consumerProguardFiles("consumer-rules.pro")
     }
