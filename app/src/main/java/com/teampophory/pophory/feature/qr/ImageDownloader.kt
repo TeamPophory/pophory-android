5928b3477a0768e74c6e17a7bf98c9be18180bf6
librarywon <librarywon@naver.com>
2023-10-17 21:13:14 +0900

    [feature/qr]: Refactor use scope

--- app/src/main/java/com/teampophory/pophory/feature/qr/ImageDownloader.kt
+++ app/src/main/java/com/teampophory/pophory/feature/qr/ImageDownloader.kt
@@ -48,20 +48,18 @@ class ImageDownloader() {
                     val downloadManager =
                         context?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
                     val query = DownloadManager.Query().setFilterById(downloadID)
-                    val cursor = downloadManager.query(query)
-                    if (cursor.moveToFirst()) {
-                        val columnIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
-                        if (DownloadManager.STATUS_SUCCESSFUL == cursor.getInt(columnIndex)) {
-                            val uriString =
-                                cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI))
-                            cursor.close()
-                            uriString?.let {
-                                callback(Uri.parse(it))
-                                return
+                    downloadManager.query(query).use { cursor ->
+                        if (cursor.moveToFirst()) {
+                            val columnIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
+                            if (DownloadManager.STATUS_SUCCESSFUL == cursor.getInt(columnIndex)) {
+                                val uriString = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI))
+                                uriString?.let {
+                                    callback(Uri.parse(it))
+                                    return
+                                }
                             }
                         }
                     }
-                    cursor.close()
                     callback(null)
                 }
             }
