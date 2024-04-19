9b84b32a49078e27b032be15d9c9a5f285413115
HyunWoo Lee (Nunu Lee) <54518925+l2hyunwoo@users.noreply.github.com>
2024-04-19 17:33:10 +0900

    Remove project warnings (#423)
    * Remove sample activity
    
    * remove activity notation in manifest file
    
    * Remove project warnings
    
    * Remove unused property warnings
    
    * Remove project warnings
    
    * remove activity layout resource

--- app/src/main/java/com/teampophory/pophory/feature/qr/ImageDownloader.kt
+++ app/src/main/java/com/teampophory/pophory/feature/qr/ImageDownloader.kt
@@ -9,25 +9,26 @@ import android.net.Uri
 import android.os.Environment
 import android.webkit.CookieManager
 import android.webkit.URLUtil
+import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
 
-class ImageDownloader() {
+class ImageDownloader {
     private var receiver: BroadcastReceiver? = null
 
     fun downloadImageFromUrl(context: Context, url: String, callback: (Uri?) -> Unit) {
-        val request = createDownloadRequest(context, url)
+        val request = createDownloadRequest(url)
         val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
         val downloadID = downloadManager.enqueue(request)
         setupDownloadCompletionReceiver(context, downloadID, callback)
     }
 
-    private fun createDownloadRequest(context: Context, url: String): DownloadManager.Request {
+    private fun createDownloadRequest(url: String): DownloadManager.Request {
         return DownloadManager.Request(Uri.parse(url)).apply {
             setMimeType("image/jpeg")
             val cookies = CookieManager.getInstance().getCookie(url)
             addRequestHeader("cookie", cookies)
             setDescription("Downloading image...")
             setTitle(URLUtil.guessFileName(url, null, "image/jpeg"))
-            allowScanningByMediaScanner()
             setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
             setDestinationInExternalPublicDir(
                 Environment.DIRECTORY_DOWNLOADS,
@@ -45,14 +46,17 @@ class ImageDownloader() {
             override fun onReceive(context: Context?, intent: Intent?) {
                 val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
                 if (downloadID == id) {
-                    val downloadManager =
-                        context?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+                    val downloadManager = context?.getSystemService<DownloadManager>() ?: return
                     val query = DownloadManager.Query().setFilterById(downloadID)
                     downloadManager.query(query).use { cursor ->
                         if (cursor.moveToFirst()) {
                             val columnIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
                             if (DownloadManager.STATUS_SUCCESSFUL == cursor.getInt(columnIndex)) {
-                                val uriString = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI))
+                                val index = cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI)
+                                if (index < 0) {
+                                    return
+                                }
+                                val uriString = cursor.getString(index)
                                 uriString?.let {
                                     callback(Uri.parse(it))
                                     return
@@ -64,7 +68,11 @@ class ImageDownloader() {
                 }
             }
         }
-        context.registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+        ContextCompat.registerReceiver(
+            context, receiver,
+            IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE),
+            ContextCompat.RECEIVER_NOT_EXPORTED
+        )
     }
 
     fun unregisterReceiver(context: Context) {
