a3ed4585272ae2dd3cf21e19ed0ed0193a1b5c10
librarywon <librarywon@naver.com>
2024-07-31 17:49:34 +0900

    [feature/force_update] domain useCase 로직 변경

--- domain/config/src/main/java/com/teampophory/pophory/config/remote/usecase/CheckAppVersionUseCase.kt
+++ domain/config/src/main/java/com/teampophory/pophory/config/remote/usecase/CheckAppVersionUseCase.kt
@@ -1,4 +1,4 @@
-package com.teampophory.pophory.domain.config.usecase
+package com.teampophory.pophory.config.remote.usecase
 
 import com.teampophory.pophory.config.remote.repository.RemoteConfigRepository
 import javax.inject.Inject
@@ -6,25 +6,8 @@ import javax.inject.Inject
 class CheckAppVersionUseCase @Inject constructor(
     private val remoteConfigRepository: RemoteConfigRepository
 ) {
-
     suspend operator fun invoke(currentVersion: String): Boolean {
-        val minRequiredVersion = remoteConfigRepository.getMinRequiredVersion()
-        return isUpdateRequired(currentVersion, minRequiredVersion)
-    }
-
-    private fun isUpdateRequired(currentVersion: String, minRequiredVersion: String): Boolean {
-        val currentVersionParts = currentVersion.split(".").map { it.toIntOrNull() ?: 0 }
-        val minVersionParts = minRequiredVersion.split(".").map { it.toIntOrNull() ?: 0 }
-
-        for (i in currentVersionParts.indices) {
-            val currentPart = currentVersionParts.getOrElse(i) { 0 }
-            val minPart = minVersionParts.getOrElse(i) { 0 }
-            if (currentPart < minPart) {
-                return true
-            } else if (currentPart > minPart) {
-                return false
-            }
-        }
-        return false
+        val latestVersion = remoteConfigRepository.getMinRequiredVersion()
+        return currentVersion < latestVersion
     }
 }
